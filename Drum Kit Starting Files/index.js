a023fd191e59a2b9887b6c368acca7f089915cd5
Kawther khlif <kawtherkhlif20@gmail.com>
2025-04-06 14:59:53 -0400

    adding an eventListener for the keyBoard + isolate char function to avoid switch repetition

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,8 +1,7 @@
-for(var i = 0 ; i< document.querySelectorAll(".drum").length ; i++){
-    document.querySelectorAll("button")[i].addEventListener("click",function(){
-var buttonInnerHtml= this.innerHTML
-switch (buttonInnerHtml) {
-    case "w":
+// function that play sounds accourding to the char
+function soundPlayer(char) {
+    switch (char) {
+        case "w":
  var audio1 = new Audio("sounds/crash.mp3")
   audio1.play();
         break;
@@ -32,12 +31,30 @@ var audio5 = new Audio("sounds/tom-2.mp3")
     audio6.play();
     break;
 
-    default:
+    case "l":
 var audio7 = new Audio("sounds/tom-4.mp3")
     audio7.play();
     break;
+    default:
+alert("You're hitting the wrong button please choose from this buttons [w,a,s,d,j,k,l")
+    break;
+   }
 }
-    })
+
+
+// an eventListener for the buttons on the screen
+for(var i = 0 ; i< document.querySelectorAll(".drum").length ; i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+var buttonInnerHtml= this.innerHTML
+soundPlayer(buttonInnerHtml)
+})
  
 }
+
+//an eventListener for the keyboard 
+document.addEventListener("keypress",function (event) {
+    soundPlayer(event.key)
+})
+
+
 
